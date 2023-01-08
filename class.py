7f2a3637b95a90e9ed83f6ac703dfaa6c5447e54
rs388 <rs388903@gmail.com>
2023-01-09 01:37:25 +0530

    add
    Signed-off-by: rs388 <rs388903@gmail.com>

---
+++ class.py
@@ -0,0 +1,11 @@
+"""Write a python Program to
+display welcome to MMMUT by using classes and objects"""
+
+class click:
+    college = ""
+    welcome = ""
+welcome_note = click()
+welcome_note.college="MMMUT"
+welcome_note.welcome="Welcome to"
+
+print(welcome_note.welcome, welcome_note.college)
